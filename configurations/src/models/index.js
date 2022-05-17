a2c75f572a99a887f82d30d434d289c54993dd2f
Dimitar Kostov <dkostowww@gmail.com>
2022-05-17 17:02:34 +0300

    Drop table relations

--- configurations/src/models/index.js
+++ configurations/src/models/index.js
@@ -5,17 +5,5 @@ const database = dbConnection;
 
 // INCLUDE ALL TABLES TO THE DB OBJECT
 database.configurations = require('../models/Configuration')(database.sequelize, database.Sequelize);
-// database.transactions = require('../models/Transaction')(database.sequelize, database.Sequelize);
-
-// DEFINE TABLE RELATIONS
-database.configurations.hasMany(database.transactions, {
-    foreignKey: 'configuration_id',
-    as: 'transactions'
-});
-
-// database.transactions.belongsTo(database.configurations, {
-//     foreignKey: 'configuration_id',
-//     as: 'configuration'
-// });
 
 module.exports = database;
